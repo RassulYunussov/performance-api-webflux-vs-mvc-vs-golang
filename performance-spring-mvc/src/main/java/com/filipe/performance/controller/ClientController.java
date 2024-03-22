5ab43eae096660bfe218d3de3c403d37f073bf1c
Rassul Yunussov <yunussov@gmail.com>
2024-03-22 22:16:56 +0200

    bump spring mvc version, add serialization routine

--- performance-spring-mvc/src/main/java/com/filipe/performance/controller/ClientController.java
+++ performance-spring-mvc/src/main/java/com/filipe/performance/controller/ClientController.java
@@ -1,8 +1,12 @@
 package com.filipe.performance.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.filipe.performance.dto.Product;
 import org.apache.http.ParseException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -11,12 +15,17 @@ import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final TypeReference<List<Product>> responseType = new TypeReference<>() {
+    };
 
     private final CloseableHttpAsyncClient apacheClient;
 
@@ -26,54 +35,50 @@ public class ClientController {
         this.apacheClient.start();
     }
 
-    
-//    @GetMapping(value = "/performance-mvc")
-//    public String getUserUsingWebfluxWebclient(@RequestParam long delay) {
-//    	return sendRequestWithApacheHttpClient(delay).join();
-//    }
-    
     @GetMapping(value = "/performance-mvc")
-    public CompletableFuture<String> getUserUsingWebfluxWebclient(@RequestParam long delay) {
-    	return sendRequestWithApacheHttpClient(delay);
-    }    
-    
-    private CompletableFuture<String> sendRequestWithApacheHttpClient(long delay) {
+    public CompletableFuture<List<Product>> getUserUsingApacheHttpClient(@RequestParam long delay) {
+        return sendRequestWithApacheHttpClient(delay);
+    }
+
+    private CompletableFuture<List<Product>> sendRequestWithApacheHttpClient(long delay) {
         CompletableFuture<org.apache.http.HttpResponse> cf = new CompletableFuture<>();
         FutureCallback<org.apache.http.HttpResponse> callback = new HttpResponseCallback(cf);
-        HttpUriRequest request = new HttpGet("http://localhost:8081/product/?delay="+delay);
+        HttpUriRequest request = new HttpGet("http://localhost:8081/product?delay=" + delay);
         apacheClient.execute(request, callback);
         return cf.thenApply(response -> {
             try {
-                return EntityUtils.toString(response.getEntity());
+                final var payload = EntityUtils.toString(response.getEntity());
+                final var result = OBJECT_MAPPER.readValue(payload, responseType);
+                return result;
             } catch (ParseException | IOException e) {
-                return e.toString();
+                return null;
             }
-        }).exceptionally(Throwable::toString);
+        }).exceptionally(e -> null);
     }
-    
+
 
     class HttpResponseCallback implements FutureCallback<org.apache.http.HttpResponse> {
 
-	    private CompletableFuture<org.apache.http.HttpResponse> cf;
-	
-	    HttpResponseCallback(CompletableFuture<org.apache.http.HttpResponse> cf) {
-	        this.cf = cf;
-	    }
-	
-	    @Override
-	    public void failed(Exception ex) {
-	        cf.completeExceptionally(ex);
-	    }
-	
-	    @Override
-	    public void completed(org.apache.http.HttpResponse result) {
-	        cf.complete(result);
-	    }
-	
-	    @Override
-	    public void cancelled() {
-	        cf.completeExceptionally(new Exception("Cancelled by http async client"));
-	    }
+        private CompletableFuture<org.apache.http.HttpResponse> cf;
+
+        HttpResponseCallback(CompletableFuture<org.apache.http.HttpResponse> cf) {
+            this.cf = cf;
+        }
+
+        @Override
+        public void failed(Exception ex) {
+            cf.completeExceptionally(ex);
+        }
+
+        @Override
+        public void completed(org.apache.http.HttpResponse result) {
+            cf.complete(result);
+        }
+
+        @Override
+        public void cancelled() {
+            cf.completeExceptionally(new Exception("Cancelled by http async client"));
+        }
     }
 }
     
